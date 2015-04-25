1640b0eacd326a83434b3fb4b8b9a9b2c00532cc
Lukas <lukas.heinrich@gmail.com>
2015-04-24 20:28:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name = 'dagger',
   author = 'Lukas Heinrich',
   version = '0.1.1',
+  packages = find_packages()
   install_requires = [
     'networkx'
   ]
