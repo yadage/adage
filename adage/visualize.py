cb183436a4647c583827083a7f334b9427900231
lukasheinrich <lukas.heinrich@gmail.com>
2016-07-21 12:08:55 +0200

    small refactoring

--- adage/visualize.py
+++ adage/visualize.py
@@ -4,12 +4,12 @@ import nodestate
 import datetime
 
 def node_visible(nodeobj,time):
-    return time > nodeobj.define_time 
+    return time > nodeobj.define_time
 
 def state_at_time(nodeobj,time):
-    defined    = nodeobj.define_time 
+    defined    = nodeobj.define_time
     submitted  = nodeobj.submit_time
-    ready_by   = nodeobj.ready_by_time 
+    ready_by   = nodeobj.ready_by_time
     if time < defined:
         return None
     if defined <= time < submitted:
@@ -20,13 +20,14 @@ def state_at_time(nodeobj,time):
         return nodeobj.state
 
 def colorize_graph(dag,normtime = None):
-    colorized = nx.DiGraph()
     allnodes = dag.nodes()
     start = min([dag.getNode(n).define_time for n in allnodes])
     stop  = max([dag.getNode(n).ready_by_time for n in allnodes])
-
     time = start + normtime*(stop-start)
+    return colorize_graph_at_time(time)
 
+def colorize_graph_at_time(dag,time):
+    colorized = nx.DiGraph()
     colorkey = {
         None : None,
         nodestate.DEFINED : 'grey',
@@ -37,18 +38,18 @@ def colorize_graph(dag,normtime = None):
 
     for node in dag.nodes():
         nodeobj = dag.getNode(node)
-        
+
         state   = state_at_time(nodeobj,time)
         color   = colorkey[state]
         visible = node_visible(nodeobj,time)
-        
+
         style = 'filled' if visible else 'invis'
         dot_attr = {'label':'{} '.format(nodeobj.name), 'style':style, 'color': color}
 
         colorized.add_node(node,dot_attr)
         for pre in dag.predecessors(node):
             colorized.add_edge(pre,node)
-    
+
     dotformat = nx.drawing.nx_pydot.to_pydot(colorized)
     dotformat.set_label(datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
     for e in dotformat.get_edges():
@@ -57,8 +58,11 @@ def colorize_graph(dag,normtime = None):
             e.set_style('invis')
     return dotformat
 
+def save_dot(dotstring,filename,fileformat):
+    with open(filename,'w') as dotoutputfile:
+        p = subprocess.Popen(['dot','-T{}'.format(fileformat),'-Gsize=18,12\!','-Gdpi=100'], stdout = dotoutputfile, stdin = subprocess.PIPE)
+        p.communicate(dotstring)
+
 def print_dag(dag,name,trackdir,time = None):
-    pngfilename = '{}/{}.png'.format(trackdir,name) 
-    with open(pngfilename,'w') as pngfile:
-        p = subprocess.Popen(['dot','-Tpng','-Gsize=18,12\!','-Gdpi=100'], stdout = pngfile, stdin = subprocess.PIPE)
-        p.communicate(colorize_graph(dag,time).to_string())
+    pngfilename = '{}/{}.png'.format(trackdir,name)
+    save_dot(colorize_graph(dag,time).to_string(),pngfilename,'png')
