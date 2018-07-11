1bca680f3920d9965231d4fcd1822d415f0bff0f
lukasheinrich <lukas.heinrich@gmail.com>
2018-07-11 06:51:52 +0300

    Bump version: 0.8.6 → 0.8.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'adage',
     author = 'Lukas Heinrich',
     author_email = 'lukas.heinrich@gmail.com',
-    version = '0.8.6',
+    version = '0.8.7',
     description = 'running dynamic DAG workflows',
     packages = find_packages(),
     install_requires = [
