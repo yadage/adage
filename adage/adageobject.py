c22b69ebbd00d909ad4c0d0b2e10951b1bfd97ac
lukasheinrich <lukas.heinrich@gmail.com>
2018-07-15 00:23:31 +0200

    simplify deserialization

--- adage/adageobject.py
+++ adage/adageobject.py
@@ -1,8 +1,7 @@
 import adage.graph
 
 class adageobject(object):
-    def __init__(self,dag = None, rules = None):
+    def __init__(self,dag = None, rules = None, applied_rules = None):
         self.dag = dag or adage.graph.AdageDAG()
         self.rules = rules or []
-        self.applied_rules = []
-    
+        self.applied_rules = applied_rules or []
