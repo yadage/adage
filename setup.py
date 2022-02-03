2486a4c560a8f105651066684118b27b9c3e9776
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-02 18:36:51 -0600

    Add lower bound of v1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ extras_require = {
     "develop": ["pyflakes", "pytest>=3.2.0", "pytest-cov>=2.5.1", "python-coveralls"],
     "viz": [
         "pydot",
-        "pygraphviz!=1.8",  # c.f. https://github.com/pygraphviz/pygraphviz/issues/395
+        "pygraphviz>=1.0,!=1.8",  # c.f. https://github.com/pygraphviz/pygraphviz/issues/395
         "pydotplus",
     ],
 }
