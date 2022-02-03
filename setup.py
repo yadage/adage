04507979a8dcc9773d4ce04084a6b1f995a2ab9f
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-02 18:41:36 -0600

    fix: Exclude pygraphviz v1.8 (#26)
    * Exclude pygraphviz v1.8 for breaking installs on Python 3.7 and older
       - c.f. https://github.com/pygraphviz/pygraphviz/issues/395
    * Set lower bound of pygraphviz v1.0
       - Untested, but realistic to ensure at least v1.0 API

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 extras_require = {
     "develop": ["pyflakes", "pytest>=3.2.0", "pytest-cov>=2.5.1", "python-coveralls"],
-    "viz": ["pydot", "pygraphviz", "pydotplus"],
+    "viz": [
+        "pydot",
+        "pygraphviz>=1.0,!=1.8",  # c.f. https://github.com/pygraphviz/pygraphviz/issues/395
+        "pydotplus",
+    ],
 }
 
 setup(extras_require=extras_require)
