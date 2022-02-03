1061f80bd33474e851573368ed91dc2452a12d7a
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-02 18:48:33 -0600

    chore: Drop pydotplus as unused since networkx v2.0 (#25)
    * Drop pydotplus from viz extra as it has unused by adage
    dependency networkx since networkx v2.0
       - c.f. https://github.com/networkx/networkx/pull/2272
    * Set lower bound on pydot of v1.2.3
       - c.f. https://github.com/networkx/networkx/pull/2272
       - > Support for the long-obsolete "pydotplus" fork has been
         > reverted back to the well-maintained parent "pydot" project...
         > All "nx_pydot" functions now explicitly require pydot >= 1.2.3,
         > which broke backwards API compatibility and resolved long-standing
         > Python 2.x issues. Sanity is restored.

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 extras_require = {
     "develop": ["pyflakes", "pytest>=3.2.0", "pytest-cov>=2.5.1", "python-coveralls"],
     "viz": [
-        "pydot",
+        "pydot>=1.2.3",  # c.f. https://github.com/networkx/networkx/pull/2272
         "pygraphviz>=1.0,!=1.8",  # c.f. https://github.com/pygraphviz/pygraphviz/issues/395
-        "pydotplus",
     ],
 }
 
